2927aa2e9fbb6aae7c7aacbc05aa0f8bddc9907e
AshwinHPrasad <ashwinprasad202@gmail.com>
2021-11-23 16:53:34 +0530

    Update settings.py

--- healthcare/healthcare/settings.py
+++ healthcare/healthcare/settings.py
@@ -109,8 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ashwinprasad202@gmail.com'
-EMAIL_HOST_PASSWORD = 'potatoInside1'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 
